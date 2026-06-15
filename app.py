9c630432d36dd4167c3881cf88c8f5a69b04fba7
Govind Turkar <govindturkar69@gmail.com>
2026-06-15 15:19:19 +0530

    Fix PyMySQL and requirements

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, flash
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 from flask_mysqldb import MySQL
 import json
 from collections import Counter
